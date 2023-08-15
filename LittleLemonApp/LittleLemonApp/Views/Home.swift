d2932e91a86dfe9c25cb5f74626353b9d8033434
arslanKara <134277229+arslanKara@users.noreply.github.com>
2023-08-15 16:14:16 +0300

    Add files via upload

---
+++ LittleLemonApp/LittleLemonApp/Views/Home.swift
@@ -0,0 +1,22 @@
+//
+//  Home.swift
+//  LittleLemonApp
+//
+//  Created by Hikmet Işıldak on 15.08.2023.
+//
+
+import SwiftUI
+
+struct Home: View {
+    
+    var body: some View {
+        MainScreen()
+            .navigationBarBackButtonHidden()
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home().environment(\.managedObjectContext, PersistenceController.shared.container.viewContext)
+    }
+}
